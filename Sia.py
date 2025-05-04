12c247012bca75bd85c324dcfbee2b4ff07cd727
Kirey <155071166+Kireyarck@users.noreply.github.com>
2025-05-04 14:55:25 -0300

    Update Sia.py

--- Sia.py
+++ Sia.py
@@ -7,11 +7,7 @@ print('     Escola a FERRAMENTA\n')
 print(' 1 - AudioX (Sonorizador) \n2 - tortoise-tts (Gerador de voz) \n 2 - TTS (Texto pra fala)\n 3 - ComfyUI  (Ia image)\n 4 - Focuus (Ia image)')
 
 
-op = input('Escolha uma opção: ')
-
-
-    
-    
+op = input('Escolha uma opção: ')    
 try:
     int(op)
 except:
@@ -19,10 +15,10 @@ except:
 
 
 
-if op == 1:
-    print(op)
-    os.system("git clone https://github.com/Kireyarck/AudioX.git" )
-    os.system("conda create -n AudioX python=3.8.20")
-    os.system("conda activate AudioX")
-    os.system("pip install git+https://github.com/ZeyueT/AudioX.git")
-    os.system("conda install -c conda-forge ffmpeg libsndfile")
+
+os.system("git clone https://github.com/Kireyarck/AudioX.git" )
+os.system("conda create -n AudioX python=3.8.20")
+os.system("conda activate AudioX")
+os.system("pip install git+https://github.com/ZeyueT/AudioX.git")
+os.system("conda install -c conda-forge ffmpeg libsndfile")
+os.system('python3 run_gradio.py --model-config model/config.json -share')
